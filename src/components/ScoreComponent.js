dfb73cbed198143cb467c7f7c2869cfea834314f
Ofir Cohen <ofirc@omcdom.hq.omc.co.il>
2024-09-01 17:31:26 +0300

    website

--- src/components/ScoreComponent.js
+++ src/components/ScoreComponent.js
@@ -158,7 +158,7 @@ const ScoreComponent = ({
       </div>
       <div className="mt-4 d-flex justify-content-center">
         <Button
-          href={`https://www.linkedin.com/shareArticle?mini=true&url=https://my-front-end-score-c4b0aa8d97ee.herokuapp.com&text=${encodeURIComponent(
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=https://tech-yourself-3a21a27e08fa.herokuapp.com&text=${encodeURIComponent(
             `היי, אחלה יוזמה לכל מפתחי האתרים שביננו!`
           )}`}
           target="_blank"
